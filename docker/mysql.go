90b5004180570a1174abcb43c255eabae5779444
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2016-09-09 13:43:22 +0200

    Add container exec start and not just create and some more logs

--- docker/mysql.go
+++ docker/mysql.go
@@ -4,6 +4,8 @@ import (
 	"crypto/md5"
 	"fmt"
 	"log"
+	"strings"
+	"unicode"
 
 	"github.com/docker/docker/api/types"
 	"golang.org/x/net/context"
@@ -11,7 +13,7 @@ import (
 
 // DeleteDatabase will try to delete a database based on prefix and domain,
 // the domain will be converted to a md5 hash.
-func DeleteDatabase(user, password, prefix, domain, container string) (bool, error) {
+func DeleteDatabase(user, password, prefix, name, container string) (bool, error) {
 	client, err := getDockerClient()
 	ctx := context.Background()
 
@@ -27,11 +29,23 @@ func DeleteDatabase(user, password, prefix, domain, container string) (bool, err
 		password = "root"
 	}
 
-	dbname := fmt.Sprintf("%s%s", prefix, fmt.Sprintf("%x", md5.Sum([]byte(domain))))
+	// Trim whitespace in container name.
+	name = strings.Map(func(r rune) rune {
+		if unicode.IsSpace(r) {
+			return -1
+		}
+		return r
+	}, name)
+
+	// Create domain based on prefix and md5.
+	dbname := fmt.Sprintf("%s%s", prefix, fmt.Sprintf("%x", md5.Sum([]byte(name))))
+
+	// Create mysql command.
 	cmd := fmt.Sprintf("mysql -u%s -p%s -e\"drop database %s\"", user, password, dbname)
 
-	log.Println(fmt.Sprintf("Trying to run: %s on container %s", cmd, container))
+	log.Println(fmt.Sprintf("Trying to run: %s on container %s with name %s", cmd, container, name))
 
+	// Exec create on container.
 	res, err := client.ContainerExecCreate(ctx, container, types.ExecConfig{
 		AttachStdin:  false,
 		AttachStdout: true,
@@ -42,7 +56,14 @@ func DeleteDatabase(user, password, prefix, domain, container string) (bool, err
 		},
 	})
 
-	log.Println(fmt.Sprintf("Response from container %s: %s", container, res.ID))
+	log.Println(fmt.Sprintf("Exec create response from container %s: %s", container, res.ID))
+
+	if err != nil {
+		return false, err
+	}
+
+	// Exec start on container.
+	err = client.ContainerExecStart(ctx, res.ID, types.ExecStartCheck{})
 
 	if err != nil {
 		return false, err
