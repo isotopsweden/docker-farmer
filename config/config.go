6c9504b0743cf58e1768387bdbbe215a39418add
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2016-09-09 11:17:06 +0200

    Move to new docker api

--- config/config.go
+++ config/config.go
@@ -34,8 +34,9 @@ func Init(s string) {
 // Config represents a config struct.
 type Config struct {
 	Database struct {
-		Prefix string
-		MD5    bool
+		Container string
+		Prefix    string
+		MD5       bool
 	}
 	Domain string
 	Docker struct {
--- docker/containers.go
+++ docker/containers.go
@@ -5,8 +5,8 @@ import (
 
 	"context"
 
-	"github.com/docker/engine-api/client"
-	"github.com/docker/engine-api/types"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
 )
 
 var (
