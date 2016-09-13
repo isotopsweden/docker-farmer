5a5e1473d74f7cb71b04ef47cbe9cd81c2586e65
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2016-09-13 09:49:07 +0200

    Add support for gitlab push events with empty after commit

--- handlers/gitlab.go
+++ handlers/gitlab.go
@@ -12,10 +12,14 @@ import (
 
 // gitlabPayload contains the object attributes object.
 type gitlabPayload struct {
+	After            string `json:"after"`
+	Before           string `json:"before"`
 	ObjectAttributes struct {
 		SourceBranch string `json:"source_branch"`
 		State        string `json:"state"`
 	} `json:"object_attributes"`
+	ObjectKind string `json:"object_kind"`
+	Ref        string `json:"ref"`
 }
 
 // GitlabHandler will handle the payload from GitLab and remove containers
@@ -32,14 +36,34 @@ func GitlabHandler(w http.ResponseWriter, r *http.Request) {
 	// Get the domain.
 	domain := config.Get().Domain
 
-	// Check merge request state.
-	if strings.ToLower(p.ObjectAttributes.State) != "merged" {
-		write(w, "GitLab: Only merge requested with state `merged` can be handle.")
-		return
+	var branch []string
+
+	// Test object kind.
+	if p.ObjectKind == "push" {
+		// A branch that is deleted don't have any after commit, but a before commit.
+		if p.Before != "0000000000000000000000000000000000000000" && p.After == "0000000000000000000000000000000000000000" {
+			// Split branch on `/`.
+			branch = strings.Split(strings.ToLower(p.Ref), "/")
+		} else {
+			write(w, "GitLab: Only push events with empty after commit and existing before commit works.")
+			return
+		}
+	} else {
+		// Check merge request state.
+		if strings.ToLower(p.ObjectAttributes.State) != "merged" {
+			write(w, "GitLab: Only merge requested with state `merged` can be handle.")
+			return
+		}
+
+		// Split branch on `/`.
+		branch = strings.Split(strings.ToLower(p.ObjectAttributes.SourceBranch), "/")
 	}
 
-	// Split branch on `/`.
-	branch := strings.Split(strings.ToLower(p.ObjectAttributes.SourceBranch), "/")
+	// Bail if branch is empty.
+	if len(branch) == 0 {
+		write(w, "GitLab: branch is empty, stopping.")
+		return
+	}
 
 	// Remove containers for the suffix.
 	suffix := fmt.Sprintf("%s.%s", branch[len(branch)-1], domain)
